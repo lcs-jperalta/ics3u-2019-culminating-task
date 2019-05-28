c8485e052912b6bc65b45242ad469a58965ec4aa
Joven Peralta <jperalta@lcs.on.ca>
2019-05-28 18:00:10 -0400

    added sprites for kirby enemies and platform tiles

---
+++ scrolling-platform-game/Bird.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bird here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bird extends Enemy
+{
+    /**
+     * Act - do whatever the Bird wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
